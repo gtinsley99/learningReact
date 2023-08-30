3ea1d48347cd53753ecc0a6f806c7cd16c087098
George Tinsley <george8s@live.co.uk>
2023-08-30 12:08:07 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return (
+  // This is the use state hook
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+
+  const [count, setCount] = useState(0);
+
+  const increaseCount = () => {
+    setCount(count + 1)
+  }
+
+  const decreaseCount = () => {
+    setCount(count - 1)
+  }
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <div>
+        {toggle ? (
+          <h1>Hello</h1>
+        ) :
+          <h1>Goodbye</h1>
+        }
+        <button className={toggle ? "red" : "blue"} onClick={handleToggle}>Click Me</button>
+        {/* Short Circuit Evaluation */}
+        {toggle && (
+          <div>
+            <h3>This is a POPUP</h3>
+            <p>Some cool text here</p>
+          </div>    
+        )}
+      </div> 
+      <div className="counter">
+        {count >= 0 ? (
+           <h1>Your number is positive</h1>
+        ) : (
+          <h1>Your number is negative</h1>
+        )}
+       
+        <div className="counterBox">
+        <button className="counterButton" onClick={decreaseCount}>-</button>
+        <h2>{count}</h2>
+        <button className="counterButton" onClick={increaseCount}>+</button>
+        </div>
+      
+      </div>   
+  </div>
+)
+};
 
 export default App;
