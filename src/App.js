934bd814adc58d93965344b3dbf47b160ba24db5
George Tinsley <george8s@live.co.uk>
2023-08-30 12:16:04 +0100

    added deploy

--- src/App.js
+++ src/App.js
@@ -12,46 +12,47 @@ function App() {
   const [count, setCount] = useState(0);
 
   const increaseCount = () => {
-    setCount(count + 1)
-  }
+    setCount(count + 1);
+  };
 
   const decreaseCount = () => {
-    setCount(count - 1)
-  }
+    setCount(count - 1);
+  };
 
-  return(
+  return (
     <div className="App">
       <div>
-        {toggle ? (
-          <h1>Hello</h1>
-        ) :
-          <h1>Goodbye</h1>
-        }
-        <button className={toggle ? "red" : "blue"} onClick={handleToggle}>Click Me</button>
+        {toggle ? <h1>Hello</h1> : <h1>Goodbye</h1>}
+        <button className={toggle ? "red" : "blue"} onClick={handleToggle}>
+          Click Me
+        </button>
         {/* Short Circuit Evaluation */}
         {toggle && (
           <div>
             <h3>This is a POPUP</h3>
             <p>Some cool text here</p>
-          </div>    
+          </div>
         )}
-      </div> 
+      </div>
       <div className="counter">
         {count >= 0 ? (
-           <h1>Your number is positive</h1>
+          <h1>Your number is positive</h1>
         ) : (
           <h1>Your number is negative</h1>
         )}
-       
+
         <div className="counterBox">
-        <button className="counterButton" onClick={decreaseCount}>-</button>
-        <h2>{count}</h2>
-        <button className="counterButton" onClick={increaseCount}>+</button>
+          <button className="counterButton" onClick={decreaseCount}>
+            -
+          </button>
+          <h2>{count}</h2>
+          <button className="counterButton" onClick={increaseCount}>
+            +
+          </button>
         </div>
-      
-      </div>   
-  </div>
-)
-};
+      </div>
+    </div>
+  );
+}
 
 export default App;
